0404e55daababdd75745a12029cde2410a4f6d81
Hung NGUYEN <hnguyen@eurecom.fr>
2021-05-05 10:09:13 +0200

    [clean][nas] clean msg folder

--- src/nas/sm/msg/PDUSessionAuthenticationComplete.c
+++ src/nas/sm/msg/PDUSessionAuthenticationComplete.c
@@ -31,79 +31,12 @@ int decode_pdu_session_authentication_complete(
     pdu_session_authentication_complete_msg*
         pdu_session_authentication_complete,
     uint8_t* buffer, uint32_t len) {
-  uint32_t decoded   = 0;
-  int decoded_result = 0;
-
-  // Check if we got a NULL pointer and if buffer length is >= minimum length
-  // expected for the message.
-  CHECK_PDU_POINTER_AND_LENGTH_DECODER(
-      buffer, PDU_SESSION_AUTHENTICATION_COMPLETE_MINIMUM_LENGTH, len);
-
-  if ((decoded_result = decode_eap_message(
-           &pdu_session_authentication_complete->eapmessage, 0,
-           buffer + decoded, len - decoded)) < 0) {
-    return decoded_result;
-  } else {
-    decoded += decoded_result;
-  }
-
-  while (len - decoded > 0) {
-    uint8_t ieiDecoded = *(buffer + decoded);
-
-    if (ieiDecoded == 0) break;
-
-    switch (ieiDecoded) {
-      case PDU_SESSION_AUTHENTICATION_COMPLETE_EPCO_IEI:
-        if ((decoded_result = decode_extended_protocol_configuration_options(
-                 &pdu_session_authentication_complete
-                      ->extendedprotocolconfigurationoptions,
-                 PDU_SESSION_AUTHENTICATION_COMPLETE_EPCO_IEI, buffer + decoded,
-                 len - decoded)) < 0)
-          return decoded_result;
-        else {
-          decoded += decoded_result;
-          pdu_session_authentication_complete->presence |=
-              PDU_SESSION_AUTHENTICATION_COMPLETE_EPCO_PRESENCE;
-        }
-        break;
-    }
-  }
-
-  return decoded;
+  return 0;
 }
 
 int encode_pdu_session_authentication_complete(
     pdu_session_authentication_complete_msg*
         pdu_session_authentication_complete,
     uint8_t* buffer, uint32_t len) {
-  uint32_t encoded   = 0;
-  int encoded_result = 0;
-
-  // Check if we got a NULL pointer and if buffer length is >= minimum length
-  // expected for the message.
-  CHECK_PDU_POINTER_AND_LENGTH_ENCODER(
-      buffer, PDU_SESSION_AUTHENTICATION_COMPLETE_MINIMUM_LENGTH, len);
-
-  if ((encoded_result = encode_eap_message(
-           pdu_session_authentication_complete->eapmessage, 0, buffer + encoded,
-           len - encoded)) < 0)
-    return encoded_result;
-  else
-    encoded += encoded_result;
-
-  if ((pdu_session_authentication_complete->presence &
-       PDU_SESSION_AUTHENTICATION_COMPLETE_EPCO_PRESENCE) ==
-      PDU_SESSION_AUTHENTICATION_COMPLETE_EPCO_PRESENCE) {
-    if ((encoded_result = encode_extended_protocol_configuration_options(
-             pdu_session_authentication_complete
-                 ->extendedprotocolconfigurationoptions,
-             PDU_SESSION_AUTHENTICATION_COMPLETE_EPCO_IEI, buffer + encoded,
-             len - encoded)) < 0) {
-      return encoded_result;
-    } else {
-      encoded += encoded_result;
-    }
-  }
-
-  return encoded;
+  return 0;
 }
