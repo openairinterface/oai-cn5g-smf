1ed1195394d28ab3683e8638007eb11bcd8e4287
Rohan <rohan.kharade@openairinterface.org>
2023-06-26 10:24:26 +0000

    seid fix in n4 session establishment request

--- src/smf_app/smf_procedure.cpp
+++ src/smf_app/smf_procedure.cpp
@@ -562,7 +562,8 @@ session_create_sm_context_procedure::send_n4_session_establishment_request() {
   //-------------------
   pfcp::fseid_t cp_fseid = {};
   smf_cfg.get_pfcp_fseid(cp_fseid);
-  cp_fseid.seid = sps->seid;
+  cp_fseid.seid      = sps->seid;
+  n4_triggered->seid = cp_fseid;
   n4_triggered->pfcp_ies.set(cp_fseid);
 
   edge dl_edge = dl_edges[0];
