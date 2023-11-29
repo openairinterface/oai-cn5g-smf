7ffe4677d4e44f01a1edabf9fc14c4a3304964c7
Tien Thinh NGUYEN <Tien-Thinh.Nguyen@eurecom.fr>
2023-11-29 11:37:13 +0100

    Fix DNN

--- src/smf_app/smf_n1.cpp
+++ src/smf_app/smf_n1.cpp
@@ -266,7 +266,8 @@ bool smf_n1::create_n1_pdu_session_establishment_accept(
       "Full DNN %s, dotted DNN %s", full_dnn.c_str(), dotted.c_str());
   sm_msg->pdu_session_establishment_accept.dnn =
       bfromcstralloc(dotted.length() + 1, "\0");
-  util::string_to_dnn(dotted, sm_msg->pdu_session_establishment_accept.dnn);
+  std::string tmp = sm_context_res.get_dnn();
+  util::string_to_dnn(tmp, sm_msg->pdu_session_establishment_accept.dnn);
 
   Logger::smf_n1().info("Encode PDU Session Establishment Accept");
   // Encode NAS message
